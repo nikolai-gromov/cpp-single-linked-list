42e278ab9caccd388f2a352b705a2a7a263fe2a7
Nikolai <nikolay.gromov.u@yandex.ru>
2023-04-14 16:56:21 +0300

    sprint 7: final project

---
+++ single-linked-list/build/CMakeFiles/single_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/single_linked_list.dir/main.cpp.o"
+  "single_linked_list"
+  "single_linked_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/single_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ single-linked-list/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikolai/practicum/cpp-single-linked-list/single-linked-list
+# Build directory: /home/nikolai/practicum/cpp-single-linked-list/single-linked-list/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
